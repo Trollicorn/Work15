491e99d2067c508e37e22159e69ca5eb2e72bb51
Trollicorn <muddin3@stuy.edu>
2018-11-17 20:06:29 -0500

    is a spork a spoon that became a fork or a fork that became a spoon

---
+++ makefile
@@ -0,0 +1,5 @@
+all: spork.c
+	gcc -o spork.out spork.c
+
+run:
+	./spork.out
--- spork.c
+++ spork.c
@@ -5,12 +5,19 @@
 #include <time.h>
 
 int main(){
-  printf("i'm need a spoon but i can only find forks");
+  printf("i need a spoon but i can only find forks\n");
   int f = fork();
   int j = 0;
   if (f) j = fork();
-  if (!j){
-    printf("%d\n", getpid());
- }
-
+  if (!j){printf("%d is trying to turn into a spoon\n",getpid());}
+  int t;
+  if (!f) {srand(time(NULL));t=rand()%15+5;/*printf("%d\n",t);*/sleep(t);exit(t);}
+  else if (!j) {srand(time(NULL)+1);t=rand()%15+5;/*printf("%d\n",t);*/sleep(t);exit(t);}
+  if (j) {
+    int n;
+    int c = wait(&n);
+    printf("%d has turned into a spork after %d seconds\n",c,WEXITSTATUS(n));
+    printf("this might work now i'll leave\n");
+  }
+  return 0;
 }
