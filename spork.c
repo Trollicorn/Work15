d58d28ca4b4a897da20a43e8fbb8fdd78ec73245
Mohammed Uddin <muddin3@stuy.edu>
2018-11-17 19:09:05 -0500

    i forgot git bash only has spoons

--- spork.c
+++ spork.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 int main(){
- int f = fork();
- int j = 0;
- if (f) j = fork();
- if (!j){
-
+  printf("i'm need a spoon but i can only find forks");
+  int f = fork();
+  int j = 0;
+  if (f) j = fork();
+  if (!j){
+    printf("%d\n", getpid());
  }
 
 }
